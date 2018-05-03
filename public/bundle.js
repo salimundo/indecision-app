e1096d7355746e0441528c1a2a4bb4c8b052182b
Sam Pellegrino <spellegrino@godaddy.com>
2018-05-03 12:50:03 -0700

    through lesson 51

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,36 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is still running!!!!!');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(1);
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](13));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](244));
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+
+const canDrink = (age) => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is still running!!!!!');
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(13));
+console.log(canDrink(244));
