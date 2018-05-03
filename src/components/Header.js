98bce89ad1705c8cd8b46c3be40820e55a78cb74
Sam Pellegrino <spellegrino@godaddy.com>
2018-05-03 16:38:10 -0700

    through lessson 56

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'React';
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{ props.title }</h1>
+      { props.subtitle && <h2>{ props.subtitle }</h2> }
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+export default Header;
