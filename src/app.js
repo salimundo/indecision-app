8d9449de5c41383cb7cb2d96e4cd0f5dafb38fe5
Sam Pellegrino <spellegrino@godaddy.com>
2018-05-07 18:43:26 -0700

    switching pc

--- src/app.js
+++ src/app.js
@@ -3,3 +3,20 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Sam';
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+
+class NewSyntax {
+  name = 'Jen';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
