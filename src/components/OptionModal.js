0a9ffd6ee139f8d5a4009c5006a3113c656bb38e
Sam Pellegrino <spellegrino@godaddy.com>
2018-05-08 18:13:41 -0700

    indecision project completed

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,17 @@ const OptionModal = (props) => (
     isOpen={ !!props.selectedOption }
     onRequestClose={ props.handleClearSelectionOption }
     contentLabel='Selected Option'
+    closeTimeoutMS={ 200 }
+    className='modal'
   >
-    <h3>Selected Option</h3>
-    { props.selectedOption && <p>{ props.selectedOption }</p> }
-    <button onClick={ props.handleClearSelectionOption }>Okay</button>
+    <h3 className='modal__title'>Selected Option</h3>
+    { props.selectedOption &&
+      <p className='modal__body'>{ props.selectedOption }</p> }
+    <button
+      className='button'
+      onClick={ props.handleClearSelectionOption }>
+        Okay
+    </button>
   </Modal>
 );
 
